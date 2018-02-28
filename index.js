64cf5e9a5d12f18d2957b82129c718c27898e590
DANIEL JOHNSTON <danjohnston93@cox.net>
2018-02-28 23:41:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue (object, key, value) {
 var  newObject = (Object.assign({recipes}, {key:value}));
   return newObject;
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object.key = value; 
+  return object; 
+}
