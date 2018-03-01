e4c8a24d93ffc19a86aea989436bdf026aba7300
DANIEL JOHNSTON <danjohnston93@cox.net>
2018-03-01 00:42:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue (object, key, value) {
   var newObject = Object.assign({}, object); 
-  newObject.key = value; 
+  newObject[key] = value; 
   return newObject; 
 }
 
